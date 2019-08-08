957283a6a31723b72392416e24a5d5044e7e82e0
Reed James <reedtjames1234@gmail.com>
2019-08-08 12:25:09 -0500

    Added more values

--- api/server.js
+++ api/server.js
@@ -5,7 +5,9 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.status(200).json({ jargonOfTheDay: process.env.JOTD });
+  res
+    .status(200)
+    .json({ jargonOfTheDay: process.env.JOTD, Reed: process.env.REED });
 });
 
 module.exports = server;
